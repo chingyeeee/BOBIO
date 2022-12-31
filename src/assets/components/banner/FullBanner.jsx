d92546578577e2e0995492c5126d78e29f3948e4
ronnie <ronnieyeung@fontrip.com>
2022-12-31 17:10:43 +0800

    modify slogan font size

--- src/assets/components/banner/FullBanner.jsx
+++ src/assets/components/banner/FullBanner.jsx
@@ -1,7 +1,7 @@
 export const FullBanner = ({ slogan }) => {
   return (
     <div className="relative flex items-center justify-center bg-fullbanner h-[calc(100vh_-_15vh)] bg-center bg-cover">
-      <p className="absolute text-n1 text-4xl md:text-6xl xl:text-8xl font-rocknroll">
+      <p className="absolute text-n1 text-h1 md:text-6xl xl:text-8xl font-rocknroll">
         {slogan}
       </p>
     </div>
