69a26e567e1e21df3c4ccde58a2c360a3c80ed53
ronnie <ronnieyeung@fontrip.com>
2022-12-31 17:10:24 +0800

    add custom fontsize

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        fullbanner: "url('/src/assets/images/homepage_banner.jpg')",
+        fullbanner: "url('/src/assets/images/homepage/homepage_banner.jpg')",
       },
       colors: {
         p1: "#FDBF52",
@@ -19,10 +19,142 @@ module.exports = {
       borderRadius: {
         btn: "1.25rem",
       },
+      fontSize: {
+        sm: [
+          "0.875rem",
+          {
+            lineHeight: "1.25rem",
+            letterSpacing: "0.05em",
+            fontWeight: "400",
+          },
+        ],
+        base: [
+          "1rem",
+          {
+            lineHeight: "1.5rem",
+            letterSpacing: "0.05em",
+            fontWeight: "400",
+          },
+        ],
+        lg: [
+          "1.125rem",
+          {
+            lineHeight: "1.75rem",
+            letterSpacing: "0.05em",
+            fontWeight: "400",
+          },
+        ],
+        "2xl": [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "0.05em",
+            fontWeight: "500",
+          },
+        ],
+        h3: [
+          "1.75rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "0.05em",
+            fontWeight: "500",
+          },
+        ],
+        h2: [
+          "2rem",
+          {
+            lineHeight: "2.5rem",
+            letterSpacing: "0.05em",
+            fontWeight: "700",
+          },
+        ],
+        h1: [
+          "2.25rem",
+          {
+            lineHeight: "2.5rem",
+            letterSpacing: "0.05em",
+            fontWeight: "900",
+          },
+        ],
+        "6xl": [
+          "3.75rem",
+          {
+            lineHeight: "1",
+            letterSpacing: "0.05em",
+            fontWeight: "900",
+          },
+        ],
+        "6xl": [
+          "6rem",
+          {
+            lineHeight: "1",
+            letterSpacing: "0.05em",
+            fontWeight: "900",
+          },
+        ],
+      },
     },
     fontFamily: {
       rocknroll: ["RocknRoll One", "sans-serif"],
       notosans: ["Noto Sans TC", "sans-serif"],
+    },
+    fontSize: {
+      sm: [
+        "0.875rem",
+        {
+          lineHeight: "1.25rem",
+          letterSpacing: "0.05em",
+          fontWeight: "400",
+        },
+      ],
+      base: [
+        "1rem",
+        {
+          lineHeight: "1.5rem",
+          letterSpacing: "0.05em",
+          fontWeight: "400",
+        },
+      ],
+      lg: [
+        "1.125rem",
+        {
+          lineHeight: "1.75rem",
+          letterSpacing: "0.05em",
+          fontWeight: "400",
+        },
+      ],
+      "2xl": [
+        "1.5rem",
+        {
+          lineHeight: "2rem",
+          letterSpacing: "0.05em",
+          fontWeight: "500",
+        },
+      ],
+      h3: [
+        "1.75rem",
+        {
+          lineHeight: "2.25rem",
+          letterSpacing: "0.05em",
+          fontWeight: "500",
+        },
+      ],
+      h2: [
+        "2rem",
+        {
+          lineHeight: "2.5rem",
+          letterSpacing: "0.05em",
+          fontWeight: "700",
+        },
+      ],
+      h1: [
+        "2.25rem",
+        {
+          lineHeight: "2.5rem",
+          letterSpacing: "0.05em",
+          fontWeight: "900",
+        },
+      ],
     },
   },
   plugins: [],
