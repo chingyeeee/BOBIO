65e6d09eeff39452d79113bf89b8db0f396877ea
ronnie <ronnieyeung@fontrip.com>
2022-12-31 16:08:33 +0800

    modify nav link css

--- src/assets/components/common/NavLink.jsx
+++ src/assets/components/common/NavLink.jsx
@@ -3,10 +3,22 @@ import { Link } from "react-router-dom";
 export const NavLink = ({ href, text, color }) => {
   return (
     <Link
-      className={`p-1.5 ${color} relative before:absolute before:w-5/6 before:mx-auto before:bg-p1 before:rounded-lg before:bottom-0 before:h-0.5 before:scale-0 hover:before:scale-100 before:transition-transform before:duration-1000 hover:text-p1 `}
+      className={`p-1.5 ${color} relative text-xl md:text-base before:absolute before:w-5/6 before:mx-auto before:bg-p1 before:rounded-lg before:bottom-0 before:h-0.5 before:scale-0 hover:before:scale-100 before:transition-transform before:duration-700 hover:text-p1 `}
       to={href}
     >
       {text}
     </Link>
   );
+};
+
+export const FooLink = ({ href, text, color }) => {
+  return (
+    <Link className={`${color} text-base hover:text-p1 `} to={href}>
+      {text}
+    </Link>
+  );
+};
+
+export const SocailLink = ({ icon }) => {
+  return <Link className="p-2 border rounded-full">{icon}</Link>;
 };
