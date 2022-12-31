10b3ccdadc58bc24e35d32683d990e4d25730650
ronnie <ronnieyeung@fontrip.com>
2022-12-31 16:08:13 +0800

    add neutral color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,17 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        fullbanner: "url('/src/assets/images/homepage_banner.jpg')",
+      },
       colors: {
         p1: "#FDBF52",
         p2: "#FFFAE7",
         p3: "#573605",
         s1: "#927858",
         s2: "#FCDBA3",
+        n1: "#ffffff",
+        n6: "#78797d",
         nav: "rgba(255, 250, 231, 0.85)",
       },
       borderRadius: {
