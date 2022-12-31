56ee48031fe786ff0ccf8967df315a0a189d95d1
ronnie <ronnieyeung@fontrip.com>
2022-12-31 16:14:20 +0800

    add homepage path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Nav } from "./assets/components/Nav";
+import { Homepage } from "./assets/components/pages/Homepage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Nav />} />
+          <Route path="/" element={<Nav />}>
+            <Route path="/" index element={<Homepage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
