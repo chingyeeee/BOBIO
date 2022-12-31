b0ab3baecc9b072da7ab90588cd8aa21c0e86b03
ronnie <ronnieyeung@fontrip.com>
2022-12-31 16:09:30 +0800

    add fullbanner component

---
+++ src/assets/components/banner/FullBanner.jsx
@@ -0,0 +1,9 @@
+export const FullBanner = ({ slogan }) => {
+  return (
+    <div className="relative flex items-center justify-center bg-fullbanner h-[calc(100vh_-_15vh)] bg-center bg-cover">
+      <p className="absolute text-n1 text-4xl md:text-6xl xl:text-8xl font-rocknroll">
+        {slogan}
+      </p>
+    </div>
+  );
+};
