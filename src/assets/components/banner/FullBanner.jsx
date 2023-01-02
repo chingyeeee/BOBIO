1988d2a0930b5273de7ddda6e9b528c9c3c5f0a2
ronnie <ronnieyeung@fontrip.com>
2023-01-02 15:43:15 +0800

    add scroll svg

--- src/assets/components/banner/FullBanner.jsx
+++ src/assets/components/banner/FullBanner.jsx
@@ -1,9 +1,21 @@
+import { CgMouse } from "react-icons/cg";
+import { BsChevronDoubleDown } from "react-icons/bs";
+
 export const FullBanner = ({ slogan }) => {
   return (
     <div className="relative flex items-center justify-center bg-fullbanner h-[calc(100vh_-_15vh)] bg-center bg-cover">
-      <p className="absolute text-n1 text-h1 md:text-5xl xl:text-8xl font-rocknroll">
+      <p className="absolute text-n1 text-h1 md:text-6xl xl:text-8xl font-rocknroll">
         {slogan}
       </p>
+      <div className="absolute bottom-12 flex flex-col items-center ">
+        <CgMouse color="#ffffff" strokeWidth={"0.5"} size={"2.5rem"} />
+        <BsChevronDoubleDown
+          className="animate-scroll-down"
+          color="#ffffff"
+          strokeWidth={"0.5"}
+          size={"1.5rem"}
+        />
+      </div>
     </div>
   );
 };
