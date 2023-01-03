40155c5da123ecabd9c1877abdd733dbf77e597e
ronnie <ronnieyeung@fontrip.com>
2023-01-03 13:18:14 +0800

    add background path

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,13 @@ module.exports = {
     extend: {
       backgroundImage: {
         fullbanner: "url('/src/assets/images/homepage/homepage_banner.jpg')",
+        joinusmobile: "url('/src/assets/images/homepage/join_us_mobile.png')",
+        joinusloop:
+          "url('/src/assets/images/homepage/join_us_loop_mobile.png')",
+        joinus: "url('/src/assets/images/homepage/join_us.png')",
+        member: "url('/src/assets/images/homepage/total_member.png')",
+        donate: "url('/src/assets/images/homepage/total_donate.png')",
+        purchase: "url('/src/assets/images/homepage/total_purchase.png')",
       },
       colors: {
         p1: "#FDBF52",
@@ -21,80 +28,6 @@ module.exports = {
       },
       borderRadius: {
         btn: "1.875rem",
-      },
-      fontSize: {
-        sm: [
-          "0.875rem",
-          {
-            lineHeight: "1.25rem",
-            letterSpacing: "0.05em",
-            fontWeight: "400",
-          },
-        ],
-        base: [
-          "1rem",
-          {
-            lineHeight: "1.5rem",
-            letterSpacing: "0.05em",
-            fontWeight: "400",
-          },
-        ],
-        lg: [
-          "1.125rem",
-          {
-            lineHeight: "1.75rem",
-            letterSpacing: "0.05em",
-            fontWeight: "400",
-          },
-        ],
-        "2xl": [
-          "1.5rem",
-          {
-            lineHeight: "2rem",
-            letterSpacing: "0.05em",
-            fontWeight: "500",
-          },
-        ],
-        h3: [
-          "1.75rem",
-          {
-            lineHeight: "2.25rem",
-            letterSpacing: "0.05em",
-            fontWeight: "500",
-          },
-        ],
-        h2: [
-          "2rem",
-          {
-            lineHeight: "2.5rem",
-            letterSpacing: "0.05em",
-            fontWeight: "700",
-          },
-        ],
-        h1: [
-          "2.25rem",
-          {
-            lineHeight: "2.5rem",
-            letterSpacing: "0.05em",
-            fontWeight: "700",
-          },
-        ],
-        "6xl": [
-          "3.75rem",
-          {
-            lineHeight: "1",
-            letterSpacing: "0.05em",
-            fontWeight: "900",
-          },
-        ],
-        "6xl": [
-          "6rem",
-          {
-            lineHeight: "1",
-            letterSpacing: "0.05em",
-            fontWeight: "900",
-          },
-        ],
       },
       fontSize: {
         caption: [
@@ -129,7 +62,7 @@ module.exports = {
             fontWeight: "500",
           },
         ],
-        xxl: [
+        h3: [
           "1.75rem",
           {
             lineHeight: "2.25rem",
@@ -149,6 +82,22 @@ module.exports = {
           "2.25rem",
           {
             lineHeight: "2.5rem",
+            letterSpacing: "0.05em",
+            fontWeight: "900",
+          },
+        ],
+        "5xl": [
+          "3.75rem",
+          {
+            lineHeight: "1",
+            letterSpacing: "0.05em",
+            fontWeight: "900",
+          },
+        ],
+        "6xl": [
+          "6rem",
+          {
+            lineHeight: "1",
             letterSpacing: "0.05em",
             fontWeight: "900",
           },
