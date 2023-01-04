e54f1359019d284247896bc43990305dd3602dee
ronnie <ronnieyeung@fontrip.com>
2023-01-04 17:34:17 +0800

    add button onclick function

--- src/assets/components/common/Button.jsx
+++ src/assets/components/common/Button.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
-export const Button = ({ href, text, icon, className = "" }) => {
+export const Button = ({ href, text, icon, className = "", handleClick }) => {
   return (
     <Link
       className={`${className} bg-p1 px-5 w-max py-2 text-lg text-p3 hover:text-n1 flex items-center gap-3 rounded-btn shadow-lv1`}
       to={href}
+      onClick={handleClick}
     >
       {text}
       {icon}
