a9ed48ca8dd7a90ef1aac136c1c0b7a1137e7dea
ronnie <ronnieyeung@fontrip.com>
2023-01-05 23:50:06 +0800

    add save userName function

--- src/assets/components/pages/DrawLots.jsx
+++ src/assets/components/pages/DrawLots.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import LotsContainer from "../../images/drawLots/lots_container.svg";
 import { Button } from "../common/Button";
 
 //drawlots step 1 ------ get user name
-const DrawUserName = ({ nextPhrase, setUserName }) => {
+const DrawUserName = ({ nextPhrase, userRef, setUserName }) => {
+  function saveUserName() {
+    if (userRef.current.value.length === 0) return;
+    setUserName(userRef.current.value);
+    nextPhrase();
+  }
+
   return (
     <div className="lg:flex lg:flex-row-reverse lg:items-center lg:gap-12">
       <div className="flex flex-col px-3 gap-8 mb-64 lg:mb-0 items-center lg:items-start lg:justify-center lg:gap-8 lg:px-0">
@@ -20,12 +26,12 @@ const DrawUserName = ({ nextPhrase, setUserName }) => {
             type="text"
             className="px-3 py-1.5 text-base font-normal text-s1 bg-white bg-clip-padding border border-solid border-s1 rounded-xl transition ease-in-out m-0 focus:text-p3 focus:bg-white focus:border-p1 focus:outline-none"
             placeholder="請輸入您的姓名"
-            onChange={(e) => setUserName(e.target.value)}
+            ref={userRef}
           />
           <Button
             className="flex-1"
             text={"開始求籤"}
-            handleClick={nextPhrase}
+            handleClick={saveUserName}
           />
         </div>
       </div>
@@ -40,6 +46,7 @@ const DrawUserName = ({ nextPhrase, setUserName }) => {
 
 const DrawLots = () => {
   const [drawPhrase, setdrawPhrase] = useState(0);
+  const userRef = useRef();
   const [userName, setUserName] = useState(null);
 
   function nextPhrase() {
@@ -49,7 +56,11 @@ const DrawLots = () => {
   return (
     <div className="container pt-16 lg:py-0 mx-auto min-h-[calc(100vh_-_173px)] md:min-h-[calc(100vh_-_156px)] flex items-center justify-center relative">
       {drawPhrase === 0 && (
-        <DrawUserName nextPhrase={nextPhrase} setUserName={setUserName} />
+        <DrawUserName
+          nextPhrase={nextPhrase}
+          userRef={userRef}
+          setUserName={setUserName}
+        />
       )}
     </div>
   );
