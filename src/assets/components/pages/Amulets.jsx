eb149827449ad798af7157190160d0109c4303df
ronnie <ronnieyeung@fontrip.com>
2023-01-12 11:41:16 +0800

    add amulets component

---
+++ src/assets/components/pages/Amulets.jsx
@@ -0,0 +1,7 @@
+const Amulets = () => {
+  return (
+    <div className="container pt-16 lg:py-0 mx-auto min-h-[calc(100vh_-_173px)] md:min-h-[calc(100vh_-_156px)] lg:min-h-screen flex items-center justify-center"></div>
+  );
+};
+
+export default Amulets;
