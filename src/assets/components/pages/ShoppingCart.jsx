710032efb0960a90ae5883a9401dc03934bd9d38
ronnie <ronnieyeung@fontrip.com>
2023-01-19 11:34:03 +0800

    add shoppingCart component

--- src/assets/components/pages/ShoppingCart.jsx
+++ src/assets/components/pages/ShoppingCart.jsx
@@ -1,6 +1,149 @@
-const ShoppingCart = () => {
+import { Button } from "../common/Button";
+import { useEffect, useState } from "react";
+import { getImageUrl } from "../utils/getImageUrl";
+
+const BuyProcess = ({ satge, text, active }) => {
   return (
-    <div className="container pt-16 lg:py-0 mx-auto min-h-[calc(100vh_-_173px)] md:min-h-[calc(100vh_-_156px)] flex items-center justify-center pb-12 lg:pb-0"></div>
+    <div
+      className={`flex flex-col lg:flex-row items-center gap-2 ${
+        active ? "" : "opacity-30"
+      }`}
+    >
+      <span className="text-n1 bg-p3 rounded-full px-3 py-1 flex items-center justify-center">
+        {satge}
+      </span>
+      <p className="text-caption md:text-lg text-p3">{text}</p>
+    </div>
+  );
+};
+
+const ShoppingCart = () => {
+  const [items, setItems] = useState([]);
+
+  function getSessionItems() {
+    const items = JSON.parse(sessionStorage.getItem("items"));
+    if (items) {
+      setItems(items);
+    }
+  }
+
+  useEffect(() => {
+    getSessionItems();
+  }, []);
+
+  return (
+    <div className="container pt-16 lg:py-0 mx-auto min-h-[calc(100vh_-_173px)] md:min-h-[calc(100vh_-_156px)] flex items-center justify-center pb-12 lg:pb-0">
+      <div className="w-full px-3 flex flex-col gap-12 md:gap-14 lg:gap-16">
+        <div className="w-full flex justify-between md:justify-around lg:justify-center lg:gap-20">
+          <BuyProcess satge={1} text={"查看購物車"} active />
+          <BuyProcess satge={2} text={"填寫收件資料"} />
+          <BuyProcess satge={3} text={"線上付款"} />
+          <BuyProcess satge={4} text={"訂單確認"} />
+        </div>
+        <div className="flex flex-col rounded-xl overflow-hidden">
+          <div className="flex bg-p1 px-6 py-3 items-center">
+            <p className="hidden md:block w-[20%] text-p3 text-normal">
+              配送方式
+            </p>
+            <p className="w-[50%] text-p3 text-normal">商品資料</p>
+            <p className="hidden md:block w-[20%] text-p3 text-normal">
+              單件價格
+            </p>
+            <p className="hidden md:block w-[15%] text-p3 text-normal">數量</p>
+          </div>
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className="flex bg-n1 px-3 md:px-6 py-3 items-center"
+            >
+              <p className="hidden md:block w-[20%] text-p3 text-normal">
+                宅配配送
+              </p>
+              <div className="w-[95%] md:w-[50%] flex gap-2 md:items-center">
+                <div className="relative rounded-xl overflow-hidden w-2/5 md:w-1/5">
+                  <img
+                    src={getImageUrl("amulets", item.zodiacImage)}
+                    alt="amulet"
+                  />
+                  <img
+                    className="absolute inset-0 m-auto w-1/2 translate-y-1"
+                    src={getImageUrl("pray/gods", item.godImage)}
+                    alt="god"
+                  />
+                </div>
+                <div className="flex flex-col w-full">
+                  <div className="flex flex-col gap-2 grow">
+                    <p className="text-caption text-p3 md:text-normal">
+                      客製化平安符
+                    </p>
+                    <div className="flex gap-2 text-xs md:text-caption text-s1 items-center ">
+                      {item.category} | {item.zodiac}
+                    </div>
+                  </div>
+                  <div className="flex justify-between md:hidden">
+                    <p className="text-p3 text-normal">NT$399</p>
+                    <div className="flex gap-2 items-center">
+                      <span
+                        id="minus"
+                        className="cursor-pointer bg-p3 text-n1 px-1.5 rounded"
+                      >
+                        −
+                      </span>
+                      <p className="text-p3 text-normal">{item.amount}</p>
+
+                      <span
+                        id="plus"
+                        className="cursor-pointer bg-p3 text-n1 px-1.5 rounded"
+                      >
+                        +
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <p className="hidden md:block w-[20%] text-p3 text-normal">
+                NT$399
+              </p>
+              <div className="hidden md:flex w-[15%] gap-2 items-center ">
+                <span
+                  id="minus"
+                  className="cursor-pointer bg-p3 text-n1 px-1.5 rounded"
+                >
+                  −
+                </span>
+                <p className="text-p3 text-normal">{item.amount}</p>
+
+                <span
+                  id="plus"
+                  className="cursor-pointer bg-p3 text-n1 px-1.5 rounded"
+                >
+                  +
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col w-[50%] ml-auto gap-2">
+          <div className="flex justify-between">
+            <p className="text-p3 text-normal md:text-lg">小計：</p>
+            <p className="text-p3 text-normal md:text-lg">
+              NT ${items.length * 399}
+            </p>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-p3 text-normal md:text-lg">運費：</p>
+            <p className="text-p3 text-normal md:text-lg">NT $60</p>
+          </div>
+          <div className="flex justify-between border-t-2 border-p3 pt-2">
+            <p className="text-p3 text-normal md:text-lg">合計：</p>
+            <p className="text-p3 text-normal md:text-lg">
+              NT ${items.length * 399 + 60}
+            </p>
+          </div>
+        </div>
+        <Button className="ml-auto" text={"下一步"} />
+      </div>
+    </div>
   );
 };
 
