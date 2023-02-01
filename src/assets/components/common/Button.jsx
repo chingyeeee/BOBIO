4054e660bff6416e7b84065eac36116ec15a5350
ronnie <ronnieyeung@fontrip.com>
2023-02-01 23:44:13 +0800

    modify button text size

--- src/assets/components/common/Button.jsx
+++ src/assets/components/common/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Button = ({ href, text, icon, className = "", handleClick }) => {
   return (
     <Link
-      className={`${className} bg-p1 px-5 w-max py-2 text-normal md:text-lg text-p3 hover:text-n1 flex items-center gap-3 rounded-btn shadow-lv1`}
+      className={`${className} bg-p1 px-5 w-max py-2 text-caption md:text-normal text-p3 hover:text-n1 flex items-center gap-3 rounded-btn shadow-lv1`}
       to={href}
       onClick={handleClick}
     >
