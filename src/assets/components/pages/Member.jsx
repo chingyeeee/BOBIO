4bf5208ca09d00bbcbf57ae7a9af44b06e6a1200
ronnie <ronnieyeung@fontrip.com>
2023-02-10 14:52:02 +0800

    add member component

---
+++ src/assets/components/pages/Member.jsx
@@ -0,0 +1,7 @@
+const Member = () => {
+  return (
+    <div className="container pt-24 lg:py-0 mx-auto min-h-[calc(100vh_-_173px)] md:h-[calc(100vh_-_156px)] flex items-center justify-center relative"></div>
+  );
+};
+
+export default Member;
